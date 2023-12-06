c827f3b428f618b27f9598a0fc14bb639e68d28b
Ayala Gonzalo <SoloEstudioTrabajo@gmail.com>
2023-12-05 23:19:56 -0300

    modificado editPutController.js

--- src/controller/admin/editPutController.js
+++ src/controller/admin/editPutController.js
@@ -8,140 +8,140 @@ const Product = require('../.././model/Product.js')
 
 const { validationResult } = require('express-validator')
 
-const editPutController = async (req, res)=>{
-	
+const editPutController = async (req, res) => {
+
 	const category = await Category.findAll()
 	const license = await License.findAll()
-	
-	
+
+
 	const error = validationResult(req)
-		
-		if(!error.isEmpty()){
-			
-			try{
-				
-				return res.render('admin/edit', {
-				
-					error: error.array(),
-					product: {...req.params, ...req.body},
-					category: category,
-					license: license
-				
-				})
-				
-			}catch(err){
-		
-				res.status(500).send(err)
-		
-			}
+
+	if (!error.isEmpty()) {
+
+		try {
+
+			return res.render('admin/edit', {
+
+				error: error.array(),
+				product: { ...req.params, ...req.body },
+				category: category,
+				license: license
+
+			})
+
+		} catch (err) {
+
+			res.status(500).send(err)
+
 		}
-	
-	
-	try{
-		
-		if(req.files.length == 2){
-			
+	}
+
+
+	try {
+
+		if (req.files.length == 2) {
+
 			const [product_front_view, product_box] = req.files.map(file => file.originalname)
-			
+
 			const images = [product_front_view, product_box]
-	
+
 			const product = await Product.update(
-				
+
 				{
 					...req.body,
 					product_front_view,
 					product_box
-					
+
 				},
 				{
-				
+
 					where: {
-						
-						id : req.params.id
-						
+
+						id: req.params.id
+
 					}
-				
+
 				})
-			
-			if(product){
-				
+
+			if (product) {
+
 				let count = 0
 				let folder = ''
-				
+
 				license.forEach(license => {
-					
-					if(license.license_id == req.body.license_id){
-						
+
+					if (license.license_id == req.body.license_id) {
+
 						folder = license.license_name
-						
+
 					}
-					
+
 				})
-				
-			
+
+
 				req.files.forEach(file => {
-			
+
 					sharp(file.buffer)
 						.resize(600)
 						.toFile(
 							path.resolve(__dirname, `../../.././public/assets/multimedia/uploads/${folder}/${images[count]}`)
 						)
-			
+
 					count++
-				
+
 				})
-				
+
 				res.redirect('/admin')
-					
-			}else{
-			
+
+			} else {
+
 				res.status(500).send('Error al actualizar el producto')
-			
+
 			}
-			
-			
-			
-		}else{
-			
+
+
+
+		} else {
+
 			const product = await Product.findOne({
-				
+
 				raw: true,
 				attributes: ['product_front_view', 'product_box'],
 				where: {
 					id: req.params.id
 				},
 				include: [
-					
+
 					{
 						model: License,
 						required: true
 					}
-					
+
 				]
 			})
-		
-			const error = [{msg: 'Las imágenes deben ser dos'}]
-			
+
+			const error = [{ msg: 'Las imágenes deben ser dos' }]
+
 			return res.render('admin/edit', {
-				
+
 				error,
-				product: {...req.params, ...req.body, ...product},
+				product: { ...req.params, ...req.body, ...product },
 				category: category,
 				license: license
-				
+
 			})
-			
+
 		}
 
-		
-		
-	}catch(err){
-		
+
+
+	} catch (err) {
+
 		res.send(err)
-		
+
 	}
-	
-	
+
+
 }
 
 
