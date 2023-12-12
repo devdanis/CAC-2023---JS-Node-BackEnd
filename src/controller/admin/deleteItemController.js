30b03b1a46b97232dae0cf1741d5ad1c419bb611
Celia Guerrero <celiaguerrero@gmail.com>
2023-12-12 19:15:47 -0300

    [FEAT] deleteItem & postCreate Controllers & ejs

--- src/controller/admin/deleteItemController.js
+++ src/controller/admin/deleteItemController.js
@@ -1,9 +1,25 @@
+const Product = require('../../model/Product')
 
-const deleteItem = (req, res) =>{
-	
-	console.log(req.params)
-	
-	res.send('shop')
+
+const deleteItem = async (req, res) =>{
+	try {
+
+		console.log(req.params)
+
+		const product = await Product.destroy({
+			where: {
+				id: req.params.id,
+			},
+		})
+
+		res.redirect('/admin')
+
+	} catch (err) {
+
+		res.send(err)
+
+	}
+
 	
 } 
 
--- src/controller/admin/postCreateController.js
+++ src/controller/admin/postCreateController.js
@@ -2,20 +2,43 @@ const { request } = require('express')
 const Category = require('../.././model/Category')
 const License = require('../.././model/License')
 const Product = require('../.././model/Product.js')
+const sharp = require ('sharp')
+const path = require('path')
+
 
 const {validationResult} = require('express-validator')
 
+
 const postCreate = async (req, res) => {
 	try {
-		console.log (req.body, req.files)
+		
 		const category = await Category.findAll()
 		const license = await License.findAll()
 		const error = validationResult(req)
 		if(!error.isEmpty()){
-			return res.render('admin/create', {error: error.array(), license, category})
-			
+			return res.render('admin/create', {
+				error: error.array(), 
+				product: { ...req.body },
+				license,
+				category})			
 		}
-		
+		if(req.files.length == 2 ) {
+			console.log (req.files)
+			const [product_front_view, product_box] = req.files.map(file => file.originalname)
+			const images = [product_front_view, product_box]
+
+			const product = await Product.create({...req.body, product_front_view, product_box})
+
+			
+			res.redirect('/admin')
+		} else {
+			const error = [{ msg : 'Las imágenes deben ser 2'}]
+			return res.render('admin/create', {
+				error: error, 
+				product: { ...req.body },
+				license,
+				category})
+		}
 	} catch(err) {
 		res.send(err)
 	}
